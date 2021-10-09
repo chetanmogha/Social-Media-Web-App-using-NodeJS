b813f59ccd8828e492a95b8f9a50e9c2f219093c
Chetan <chetan.mogha@gmail.com>
2021-10-10 02:46:09 +0530

    Add user controller and profile routes

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,10 @@ const router =express.Router();
 const home_controller=require('../controllers/home_controller')
 
 router.get('/',home_controller.home)
+
+router.use('/users',require('./users'))
+
+// for any other routes, access from here
+// router.use('/routerName',require('router path')) 
 console.log("Router Started")
 module.exports=router;
