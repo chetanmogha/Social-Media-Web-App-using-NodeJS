e97df40f469c21abb3261cd421e09934a20e3680
Chetan <chetan.mogha@gmail.com>
2021-10-10 13:49:43 +0530

    Configure the views engine

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app=express();
 
 // use express router
 app.use('/',require('./routes/index'))
+
+// setup the views engine
+
 app.set('view engine','ejs')
 app.set('views','./views');
 app.listen(8000,(err)=>{
